b527dcfcb3070cd11c713c1da1708e372941ef67
Simone <75372731+smosciatti@users.noreply.github.com>
2021-03-07 07:41:30 +0100

    Add files via upload
    little bug fix 
    if parameter = 0 calculation are executed, variable type in main should be same (long) than method return

--- SpeedConverter/src/Main.java
+++ SpeedConverter/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args){
 
-        double s = SpeedConverter.toMilesPerHour(1.5);
+        long s = SpeedConverter.toMilesPerHour(1.5);
         System.out.println("first method " + s);
         SpeedConverter.printConversion(-2);
     }
